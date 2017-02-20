ded91ef66d20121a36f44764a2527697516b3622
DanielMeixner <DanielMeixner@DESKTOP-NIK567Q>
2017-02-20 14:44:30 +0100

    Added UI Test

---
+++ CodedUITestProject/CodedUITest1.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UITesting;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.VisualStudio.TestTools.UITest.Input;
+using Microsoft.VisualStudio.TestTools.UITest.Extension;
+using Microsoft.VisualStudio.TestTools.UITesting.HtmlControls;
+using Microsoft.VisualStudio.TestTools.UITesting.DirectUIControls;
+using Microsoft.VisualStudio.TestTools.UITesting.WindowsRuntimeControls;
+using Keyboard = Microsoft.VisualStudio.TestTools.UITesting.Keyboard;
+
+
+namespace CodedUITestProject
+{
+    /// <summary>
+    /// Summary description for CodedUITest1
+    /// </summary>
+    [CodedUITest(CodedUITestType.WindowsStore)]
+    public class CodedUITest1
+    {
+        public CodedUITest1()
+        {
+            
+        }
+
+        [TestMethod]
+        public void CodedUITestMethod1()
+        {
+            // To generate code for this test, select "Generate Code for Coded UI Test" from the shortcut menu and select one of the menu items.
+            XamlWindow.Launch("7e46422f-a8d0-44f3-8fb2-8f35c805e502_rzfhbrsrjxcbe!App");
+            Gesture.Tap(UIMap.UIRefrigeratorControlUWindow.UIBtnFreezeButton);
+            this.UIMap.AssertMethod1();
+           
+
+        }
+        [TestMethod]
+        public void CodedUITestMethod2()
+        {
+            XamlWindow.Launch("7e46422f-a8d0-44f3-8fb2-8f35c805e502_rzfhbrsrjxcbe!App");
+            Gesture.Tap(UIMap.UIRefrigeratorControlUWindow.UIBtnFreezeButton);
+
+            Gesture.PressAndHold(UIMap.UIRefrigeratorControlUWindow.UIBtnFreezeButton);
+            Gesture.Tap(UIMap.UIRefrigeratorControlUWindow.UIBtnTempButton);
+            
+            this.UIMap.AssertMethod2();
+        }
+
+        #region Additional test attributes
+
+        // You can use the following additional attributes as you write your tests:
+
+        ////Use TestInitialize to run code before running each test 
+        //[TestInitialize()]
+        //public void MyTestInitialize()
+        //{        
+        //    // To generate code for this test, select "Generate Code for Coded UI Test" from the shortcut menu and select one of the menu items.
+        //}
+
+        ////Use TestCleanup to run code after each test has run
+        //[TestCleanup()]
+        //public void MyTestCleanup()
+        //{        
+        //    // To generate code for this test, select "Generate Code for Coded UI Test" from the shortcut menu and select one of the menu items.
+        //}
+
+        #endregion
+
+        /// <summary>
+        ///Gets or sets the test context which provides
+        ///information about and functionality for the current test run.
+        ///</summary>
+        public TestContext TestContext
+        {
+            get
+            {
+                return testContextInstance;
+            }
+            set
+            {
+                testContextInstance = value;
+            }
+        }
+        private TestContext testContextInstance;
+
+        public UIMap UIMap
+        {
+            get
+            {
+                if ((this.map == null))
+                {
+                    this.map = new UIMap();
+                }
+
+                return this.map;
+            }
+        }
+
+        private UIMap map;
+    }
+}
